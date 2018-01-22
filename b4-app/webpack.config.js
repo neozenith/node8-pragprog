12b6265881a7f2e6166f61cee374cfe4e116870a
Josh Peak <neozenith.dev@gmail.com>
2018-01-22 08:38:36 +0100

    Add Clientside javascript and Webpack exercise

---
+++ b4-app/webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const distDir = path.resolve(__dirname, 'dist');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './entry.js',
+
+	output: {
+		filename: 'bundle.js',
+		path: distDir
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader','css-loader']
+			},
+			{
+				test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+				loader: 'url-loader?limit=100000',
+			}
+		],
+	},
+
+	devServer: {
+		contentBase: distDir,
+		port: 60800
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Better Book Bundle Builder'
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		})
+	]
+};
