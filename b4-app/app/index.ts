bd344d2f9bb292b1a129ff9a20f2e173a3e88fe1
Josh Peak <neozenith.dev@gmail.com>
2018-01-22 12:18:11 +0100

    Add Handlebars HTML templating

--- b4-app/app/index.ts
+++ b4-app/app/index.ts
@@ -2,10 +2,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import * as templates from './templates.ts';
 
-document.body.innerHTML = templates.main;
+document.body.innerHTML = templates.main();
 
 const mainElement = document.body.querySelector('.b4-main');
 const alertsElement = document.body.querySelector('.b4-alerts');
 
-mainElement.innerHTML = templates.welcome;
-alertsElement.innerHTML = templates.alert;
+mainElement.innerHTML = templates.welcome();
+alertsElement.innerHTML = templates.alert({
+	type: 'info',
+	message: 'Handlebars is working'
+});
--- b4-app/app/templates.ts
+++ b4-app/app/templates.ts
@@ -1,26 +1,28 @@
+import * as HandleBars from '../node_modules/handlebars/dist/handlebars.js';
 
-export const main = `
+export const main = HandleBars.compile(`
 	<div class="container">
 		<h1>B4 - Book Bundler</h1>
 		<div class="b4-alerts"></div>
 		<div class="b4-main"></div>
 	</div>
-`;
+`);
 
 
-export const welcome = `
+export const welcome = HandleBars.compile(`
 	<div class="jumbotron">
 		<h1>Welcome!</h1>
 		<p>B4 is an aplication for creating book bundles</p>
 	</div>
-`;
+`);
 
 
-export const alert = `
-	<div class="alert alert-success alert-dismissable fade in" role="alert">
+export const alert = HandleBars.compile(`
+	<div class="alert alert-{{type}} alert-dismissable fade in" role="alert">
 		<button class="close" data-dismiss="alert" aria-label="Close">
 			<span aria-hidden="true">&times;</span>
 		</button>
+		{{message}}
 		<strong>Success!</strong> Bootstrap is working.
 	</div>
-`;
+`);
