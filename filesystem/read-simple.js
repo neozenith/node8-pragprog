503ae2a048c8cb4d5c3ece705ea2770162aaaf86
Josh Peak <neozenith.dev@gmail.com>
2018-01-22 08:33:19 +0100

    Add filesystem event watching exercise

---
+++ filesystem/read-simple.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+'use strict'
+const fs = require('fs');
+fs.createReadStream(process.argv[2])
+	.on('data', chunk => process.stdout.write(chunk))
+	.on('error', err => process.stderr.write(`ERROR: ${err.message}\n`));
+
---
+++ filesystem/watcher.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+'use strict'
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+
+const filename = process.argv[2];
+
+if (!filename) {
+	throw Error('A file to watch must be specified');
+}
+
+fs.watch(filename, (eventType, filename) => {
+		console.log(eventType, filename);
+	const ls = spawn('ls', ['-la', filename]);
+	let output = '';
+
+	ls.stdout.on('data', chunk => output += chunk);
+	ls.on('close', () => {
+		const parts = output.split(/\s+/);
+		console.log([parts[0], parts[4], parts[8]]);
+	});
+});
+console.log(`Now watching ${filename}`);
