abda1fab030b497fdcf8f70c6f5793992dc4651d
Josh Peak <neozenith.dev@gmail.com>
2018-01-26 13:59:40 +0100

    Add TS linting start work on grabbing bundles to display in view

--- b4-app/app/index.ts
+++ b4-app/app/index.ts
@@ -1,5 +1,5 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import * as templates from './templates.ts';
 
 document.body.innerHTML = templates.main();
@@ -7,6 +7,20 @@ document.body.innerHTML = templates.main();
 const mainElement = document.body.querySelector('.b4-main');
 const alertsElement = document.body.querySelector('.b4-alerts');
 
+const listBundles = bundles => {
+	mainElement.innerHTML = templates.listBundles({bundles});
+};
+
+const getBundles = async () => {
+	const esRes = await fetch('es/b4/bundle/_search?size=1000');
+
+	const esResBody = await esRes.json();
+
+	return esResBody.hits.hits.map(hit => ({
+		id: hit._id,
+		name: hit._source.name,
+	}));
+};
 
 /**
  *  Use Window Location hash to show the specified view
@@ -19,8 +33,12 @@ const showView = async () => {
 		case '#welcome':
 			mainElement.innerHTML = templates.welcome();
 			break;
+		case '#list-bundles':
+			const bundles = await getBundles();
+			listBundles(bundles);
+			break;
 		default:
-			//unrecognised view
+			// unrecognised view
 			throw Error(`Unrecognised view: ${view}`);
 	}
 };
