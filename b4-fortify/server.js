99fe055baad662e5301dacb79888324a6cda46fd
Josh Peak <neozenith.dev@gmail.com>
2018-04-24 14:57:39 +1000

    tidy up code

--- b4-fortify/server.js
+++ b4-fortify/server.js
@@ -5,28 +5,25 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/jwnode2 for more book information.
-***/
+ ***/
 'use strict';
 const pkg = require('./package.json');
-const {URL} = require('url');
+const { URL } = require('url');
 const path = require('path');
 
 // nconf configuration.
 const nconf = require('nconf');
 nconf
-  .argv()
-  .env('__')
-  .defaults({'NODE_ENV': 'development'});
+	.argv()
+	.env('__')
+	.defaults({ NODE_ENV: 'development' });
 
 const NODE_ENV = nconf.get('NODE_ENV');
 const isDev = NODE_ENV === 'development';
-nconf
-  .defaults({'conf': path.join(__dirname, `${NODE_ENV}.config.json`)})
-  .file(nconf.get('conf'));
+nconf.defaults({ conf: path.join(__dirname, `${NODE_ENV}.config.json`) }).file(nconf.get('conf'));
 
 const serviceUrl = new URL(nconf.get('serviceUrl'));
-const servicePort =
-    serviceUrl.port || (serviceUrl.protocol === 'https:' ? 443 : 80);
+const servicePort = serviceUrl.port || (serviceUrl.protocol === 'https:' ? 443 : 80);
 
 // Express and middleware.
 const express = require('express');
@@ -40,15 +37,17 @@ app.get('/api/version', (req, res) => res.status(200).json(pkg.version));
 
 // Serve webpack assets.
 if (isDev) {
-  const webpack = require('webpack');
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpackConfig = require('./webpack.config.js');
-  app.use(webpackMiddleware(webpack(webpackConfig), {
-    publicPath: '/',
-    stats: {colors: true},
-  }));
+	const webpack = require('webpack');
+	const webpackMiddleware = require('webpack-dev-middleware');
+	const webpackConfig = require('./webpack.config.js');
+	app.use(
+		webpackMiddleware(webpack(webpackConfig), {
+			publicPath: '/',
+			stats: { colors: true }
+		})
+	);
 } else {
-  app.use(express.static('dist'));
+	app.use(express.static('dist'));
 }
 
-app.listen(servicePort, () => console.log('Ready.'));
+app.listen(servicePort, () => console.log(`Ready @ ${serviceUrl}`));
